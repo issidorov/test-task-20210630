01e5058adb8600c79a1c36c8bc1b731777d3a623
Ivan Sidorov <nobxod@gmail.com>
2021-07-02 11:17:00 +0300

    Display logs - charts

--- views/site/index.php
+++ views/site/index.php
@@ -9,6 +9,68 @@
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
+    <div class="row">
+        <div class="col-md-6">
+            <?= \dosamigos\chartjs\ChartJs::widget([
+                'type' => 'line',
+                'options' => [
+                    'height' => 150,
+                    'width' => 300,
+                ],
+                'data' => [
+                    'labels' => ["January", "February", "March", "April", "May", "June", "July"],
+                    'datasets' => [
+                        [
+                            'label' => "My First dataset",
+                            'backgroundColor' => "rgba(179,181,198,0.2)",
+                            'borderColor' => "rgba(179,181,198,1)",
+                            'pointBackgroundColor' => "rgba(179,181,198,1)",
+                            'pointBorderColor' => "#fff",
+                            'pointHoverBackgroundColor' => "#fff",
+                            'pointHoverBorderColor' => "rgba(179,181,198,1)",
+                            'data' => [65, 59, 90, 81, 56, 55, 40]
+                        ],
+                    ]
+                ]
+            ]); ?>
+        </div>
+        <div class="col-md-6">
+            <?= \dosamigos\chartjs\ChartJs::widget([
+                'type' => 'line',
+                'options' => [
+                    'height' => 150,
+                    'width' => 300,
+                ],
+                'data' => [
+                    'labels' => ["January", "February", "March", "April", "May", "June", "July"],
+                    'datasets' => [
+                        [
+                            'label' => "My First dataset",
+                            'backgroundColor' => "rgba(179,181,198,0.2)",
+                            'borderColor' => "rgba(179,181,198,1)",
+                            'pointBackgroundColor' => "rgba(179,181,198,1)",
+                            'pointBorderColor' => "#fff",
+                            'pointHoverBackgroundColor' => "#fff",
+                            'pointHoverBorderColor' => "rgba(179,181,198,1)",
+                            'data' => [65, 59, 90, 81, 56, 55, 40]
+                        ],
+                        [
+                            'label' => "My Second dataset",
+                            'backgroundColor' => "rgba(255,99,132,0.2)",
+                            'borderColor' => "rgba(255,99,132,1)",
+                            'pointBackgroundColor' => "rgba(255,99,132,1)",
+                            'pointBorderColor' => "#fff",
+                            'pointHoverBackgroundColor' => "#fff",
+                            'pointHoverBorderColor' => "rgba(255,99,132,1)",
+                            'data' => [28, 48, 40, 19, 96, 27, 100]
+                        ]
+                    ]
+                ]
+            ]); ?>
+        </div>
+    </div>
+
+
     <?php $form = \yii\bootstrap\ActiveForm::begin(['action' => ['site/index'], 'method' => 'get']) ?>
     <div class="row">
         <div class="col-md-2">
